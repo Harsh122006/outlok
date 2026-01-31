8d1c10f93fe440fc962a524594923fa529b7f451
Harsh Kushwaha <harshkushwaha9695@gmail.com>
2026-01-31 17:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import asyncio
 from fastapi import FastAPI
-from bot import start_bot
+from telegram_bot import start_bot
 
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup_event():
-    print("🚀 FastAPI startup")
-    asyncio.create_task(start_bot())
-    print("🤖 Telegram bot task started")
+async def startup():
+    print("🚀 App startup")
+    asyncio.create_task(start_bot())  # run telegram bot in background
+    print("🤖 Telegram bot started")
 
 @app.get("/")
 async def root():
